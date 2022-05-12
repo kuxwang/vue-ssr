f230d58e7b0c36603645795b6396c22870f2951c
wangcw <wangcw@greatld.com>
2022-05-12 21:19:36 +0800

    chore: add vueuse postcss-px-to-viewport  vant  axios

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: [
+    require('postcss-px-to-viewport')({ 
+      viewportWidth: 750,
+      viewportHeight: 1334, 
+      unitPrecision: 3, 
+      viewportUnit: "vw",
+      selectorBlackList: ['.ignore'], // 指定不转换为视窗单位的类
+      minPixelValue:1,
+      mediaQuery:false,
+    })
+  ],
+
+
+
+}
